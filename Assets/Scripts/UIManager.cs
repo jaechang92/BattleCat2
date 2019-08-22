f35dfe75a118da7e1c189a81e6ff1210927fc862
jaechang <jaechang0721k@gmail.com>
2019-08-22 19:21:12 +0900

    UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct UIBoolSet
+{
+    public bool mainMenu;
+    public bool stageSelect;
+}
+
+public class UIManager : MonoBehaviour {
+
+
+    public UIBoolSet UISet;
+    public List<GameObject> UIList;
+	
+	void Start () {
+        UISet.mainMenu = true;
+        UISet.stageSelect = false;
+	}
+	
+	
+	void Update () {
+        
+	}
+
+
+    public void ButtonClike(string obj)
+    {
+        Debug.Log("in");
+        switch (obj)
+        {
+            case "Start":
+                UIList[0].SetActive(false);
+                UIList[1].SetActive(true);
+                break;
+            case "Option":
+                break;
+            
+
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/UIScrollControll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+public class UIScrollControll : MonoBehaviour
+{
+
+
+    public GameObject contents;
+
+    public int contentNum = 0;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
