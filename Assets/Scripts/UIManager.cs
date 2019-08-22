a827e5044e53a49c78fc5d9fc0374a97ae317a0f
jaechang <jaechang0721k@gmail.com>
2019-08-22 20:22:24 +0900

    Scroll Snap

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 [System.Serializable]
 public struct UIBoolSet
 {
-    public bool mainMenu;
+    public bool startAndOption;
     public bool stageSelect;
+    public bool main;
+    public bool battleStage;
 }
 
 public class UIManager : MonoBehaviour {
 
 
     public UIBoolSet UISet;
+    
     public List<GameObject> UIList;
 	
 	void Start () {
-        UISet.mainMenu = true;
+        UISet.startAndOption = true;
         UISet.stageSelect = false;
+
+        UIList[0].SetActive(true);
 	}
 	
 	
@@ -32,13 +37,35 @@ public class UIManager : MonoBehaviour {
         switch (obj)
         {
             case "Start":
+                UISet.startAndOption = false;
+                UISet.stageSelect = true;
+
                 UIList[0].SetActive(false);
                 UIList[1].SetActive(true);
                 break;
             case "Option":
                 break;
-            
+            case "World":
+                UISet.stageSelect = false;
+                UISet.main = true;
 
+                UIList[1].SetActive(false);
+                UIList[2].SetActive(true);
+                break;
+            case "Future":
+                break;
+            case "CatSchool":
+                break;
+
+
+            case "BattleMenu":
+                break;
+            case "PowerUp":
+                break;
+            case "CharacterForming":
+                break;
+            case "BackSapce":
+                break;
             default:
                 break;
         }
