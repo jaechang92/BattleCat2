3dcf87ca8775330b3412dd16a6cce9a990decd5d
jaechang <jaechang0721k@gmail.com>
2019-09-15 05:23:00 +0900

    BackGround

---
+++ Assets/Scripts/ChildControll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildControll : MonoBehaviour {
+
+    [SerializeField]
+    public Vector2 cellSize, spacing;
+
+    public List<GameObject> childSanpObject;
+
+    private RectTransform nowRect;
+    private void Start()
+    {
+        for (int i = 0; i < childSanpObject.Count; i++)
+        {
+            nowRect = childSanpObject[i].GetComponent<RectTransform>();
+            nowRect.sizeDelta = cellSize;
+            nowRect.anchoredPosition = new Vector2((cellSize.x + spacing.x) * i, 0);
+
+        }
+    }
+
+
+}
